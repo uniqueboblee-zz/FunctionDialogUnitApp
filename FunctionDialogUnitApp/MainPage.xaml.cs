cb5ea8ffe23fa54cdc2ba97435b727e9f507e58e
yongbo <v-yongbl@microsoft.com>
2018-08-27 20:20:04 +0800

    Add project files.

---
+++ FunctionDialogUnitApp/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Core;
+using Windows.UI.Core;
+using Windows.UI.Popups;
+using System.Diagnostics;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace FunctionDialogUnitApp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private string command = "";
+        private ShowFunctionDialog functionDialog;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            Init();
+        }
+
+        private async void Init()
+        {
+            command = "Power OFF";
+            functionDialog = new ShowFunctionDialog()
+            {
+                Command = command
+            };
+            await functionDialog.ShowAsync();
+        }
+    }
+}
---
+++ FunctionDialogUnitApp/ShowFunctionDialog.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Content Dialog item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace FunctionDialogUnitApp
+{
+    public sealed partial class ShowFunctionDialog : ContentDialog
+    {
+        public ShowFunctionDialog()
+        {
+            this.InitializeComponent();
+        }
+
+        public string Command{ get; set; }
+        private void OK_ButtonClick(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+        }
+    }
+}
